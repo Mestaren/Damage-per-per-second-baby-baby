904a349407383d0e7386e3951414659e84f02ca7
Lucas Escobar <73233341+Mestaren@users.noreply.github.com>
2023-03-29 14:12:06 +0200

    Påbörjad med Iframes

--- SonicGaem/Assets/Scripts/PlayserKnockbackScript.cs
+++ SonicGaem/Assets/Scripts/PlayserKnockbackScript.cs
@@ -10,6 +10,15 @@ public class PlayserKnockbackScript : MonoBehaviour
     LayerMask player;
     RaycastHit hit;
 
+    [Header("DennisBajs123")]
+
+    public Color flashColour;
+    public Color regularColour;
+    public float flashDuration;
+    public int numberOfFlashes;
+    public Collider triggerCollider;
+    public SpriteRenderer mySprite;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,6 +47,22 @@ public class PlayserKnockbackScript : MonoBehaviour
 
     void KnockBack()
     {
+        StartCoroutine(FlashCo());
         Player.transform.position += transform.up * Time.deltaTime * Knockbackforce;
+    }
+
+    private IEnumerator FlashCo()
+    {
+        int temp = 0;
+        triggerCollider.enabled = false;
+        while(temp < numberOfFlashes)
+        {
+            mySprite.color = flashColour;
+            yield return new WaitForSeconds(flashDuration);
+            mySprite.color = regularColour;
+            yield return new WaitForSeconds(flashDuration);
+            temp++;
+        }
+        triggerCollider.enabled = true;
     }
 }
---
+++ SonicGaem/Assets/Scripts/iFrames.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iFrames : MonoBehaviour
+{
+    public Color flashColour;
+    public Color regularColour;
+    public float flashDuration;
+    public int numberOfFlashes;
+    public Collider triggerCollider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
