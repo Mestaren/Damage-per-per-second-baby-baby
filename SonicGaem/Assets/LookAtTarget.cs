3b45feb8d325bb5b49e2306b052bf3977cabfb08
ISHE04001 <77394101+ISHE04001@users.noreply.github.com>
2023-04-26 14:16:41 +0200

    rotate

--- SonicGaem/Assets/LookAtTarget.cs
+++ SonicGaem/Assets/LookAtTarget.cs
@@ -5,32 +5,11 @@ using UnityEngine;
 public class LookAtTarget : MonoBehaviour
 {
     public Transform Target;
-    public float Speed = 1f;
 
-    private Coroutine LookCoroutine;
 
-    public void StartRotating()
+    private void Update()
     {
-        if (LookCoroutine != null)
-        {
-            StopCoroutine(LookCoroutine);
-        }
-
-        LookCoroutine = StartCoroutine(LookAt());
-    }
-
-    private IEnumerator LookAt()
-    {
-        Quaternion lookRotation = Quaternion.LookRotation(Target.position - transform.position);
-
-        float time = 0;
-        while (time < 0)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, time);
-
-            time += Time.deltaTime * Speed;
-
-            yield return null;
-        }
+        transform.LookAt(Target);
+        
     }
 }
